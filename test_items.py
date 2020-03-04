71a6abb31e8f03e542a40c5874ba20b338dfd1d0
Anastasiya <Nadtochy@llcglobaladvisors.com>
2020-03-04 16:55:09 +0300

    Homework 4 to assert button

---
+++ test_items.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+import time
+
+link ='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_add_to_basket_button(browser):
+    browser.get(link)
+    time.sleep(30) #sleep для проверки правильности инициализации браузера
+    assert browser.find_element_by_css_selector('.btn-add-to-basket').isEnabled
